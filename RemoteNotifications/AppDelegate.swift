30601b6cd0aab2c1623db552218a7dff09a7dbb3
Magnus Ödman <magnus@Magnuss-iMac.local>
2017-02-20 22:05:21 +0100

    Add changes since last commmit

--- RemoteNotifications/AppDelegate.swift
+++ RemoteNotifications/AppDelegate.swift
@@ -16,6 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        let settings = UIUserNotificationSettings(forTypes: .Alert, categories: nil)
+        UIApplication.sharedApplication().registerUserNotificationSettings(settings)
+        UIApplication.sharedApplication().registerForRemoteNotifications()
         return true
     }
 
@@ -39,6 +42,44 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+    
+    func application(application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: NSData) {
+        let encoded = deviceToken.base64EncodedDataWithOptions(.Encoding64CharacterLineLength)
+        print("Registered for remote notifications")
+        print("Base64 encoded device token:\n\(encoded)")
+        print("Hex encoded device token: \(convertToHex(deviceToken))")
+                
+        NSNotificationCenter.defaultCenter().postNotificationName("UUIDUpdated", object: nil, userInfo: ["uuid": convertToHex(deviceToken)])
+    }
+    
+    func application(application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: NSError) {
+        print("Failed to register for remote notification.\nError was: \(error)")
+    }
+    
+    func application(application: UIApplication, didReceiveRemoteNotification userInfo: [NSObject : AnyObject]) {
+        print("Received remote notification")
+        print(userInfo)
+        
+        let alertController = UIAlertController(title: "Remote notification", message: "Received remote notification", preferredStyle: .Alert)
+        
+        self.window?.rootViewController?.presentViewController(alertController, animated: true, completion: {
+            
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(NSEC_PER_SEC)), dispatch_get_main_queue(), { 
+                alertController.dismissViewControllerAnimated(true, completion: nil)
+            })
+            
+        })
+        
+    }
+    
+    func convertToHex(data: NSData) -> String {
+        let tokenChars = UnsafePointer<CChar>(data.bytes)
+        var tokenString = ""
+        for i in 0..<data.length {
+            tokenString += String(format: "%02.2hhx", arguments: [tokenChars[i]])
+        }
+        return tokenString
     }
 
 
--- RemoteNotifications/ViewController.swift
+++ RemoteNotifications/ViewController.swift
@@ -10,9 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var uuidTextField: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(updateTextField), name: "UUIDUpdated", object: nil)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +22,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func updateTextField(notification: NSNotification) {
+        if let uuid = notification.userInfo!["uuid"] as? String {
+            uuidTextField.text = uuid
+        }
+        
+    }
 
+    @IBAction func copyToClipboard(sender: AnyObject) {
+        UIPasteboard.generalPasteboard().string = uuidTextField.text
+        let activityViewController = UIActivityViewController(activityItems: [uuidTextField.text! as NSString], applicationActivities: nil)
+        presentViewController(activityViewController, animated: true, completion: {})
+        
+    }
 }
 
